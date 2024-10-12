a948e70f8fbc7de94af9bc363497bafc5c161646
prajjwal98dubey <prajjwaldubey003@gmail.com>
2024-10-12 12:54:08 +0530

    add debouncing in search

--- client/src/components/SearchModal.jsx
+++ client/src/components/SearchModal.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BackDrop from "./BackDrop";
 import axios from "axios";
 import { SEARCH_API } from "../apis/backendapi";
@@ -11,8 +11,9 @@ const SearchModal = ({ setIsSearchModalOpen }) => {
   const [searchText, setSearchText] = useState("");
   const [searchResult, setSearchResult] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+
   const handleSearchQuery = async () => {
-    if (searchText === "") return alert("search something to proceed...");
+    // if (searchText === "") return alert("search something to proceed...");
     const { data } = await axios.get(SEARCH_API + `?text=${searchText}`, {
       headers: {
         "Content-Type": "application/json",
@@ -21,6 +22,28 @@ const SearchModal = ({ setIsSearchModalOpen }) => {
     setSearchResult(data);
     setIsLoading(false);
   };
+
+  const debounce = (func, delay) => {
+    return () => {
+      let timer = setTimeout(() => {
+        func();
+      }, delay);
+      return timer;
+    };
+  };
+
+  const debounced = debounce(handleSearchQuery, 500);
+  useEffect(() => {
+    let timer = null;
+    if (searchText === "") setSearchResult([]);
+    if (searchText !== "") {
+      timer = debounced();
+    }
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchText]);
+
   return (
     <div className="absolute">
       <div onClick={() => setIsSearchModalOpen(false)}>
@@ -43,7 +66,7 @@ const SearchModal = ({ setIsSearchModalOpen }) => {
             search
           </button>
         </span>
-        {!isLoading && (
+        {!isLoading && searchResult.length >=1 && (
           <div
             className={`w-[400px] ${
               searchResult.length > 7
@@ -51,53 +74,59 @@ const SearchModal = ({ setIsSearchModalOpen }) => {
                 : "h-fit"
             }  p-2 absolute top-12  font-rubik transform  border border-gray-400 shadow-sm shadow-gray-400 rounded-md z-10 bg-[#2a2929]`}
           >
-            {searchResult.map((item) => (
-              <Link
-                key={item._id}
-                to={item.category === "people" ? `/u/${item.name}` : `/c/${item._id}`}
-              >
-                <div className="w-full h-fit p-1 hover:bg-[#464545] cursor-pointer flex rounded-md m-1">
-                  {item.category === "people" ? (
-                    item.photo !== "" ? (
-                      <div>
-                        <img
-                          className="w-[40px] h-[40px] rounded-full"
-                          src={item.photo}
-                          alt="loading"
-                        />
-                      </div>
-                    ) : (
-                      <div className="w-[40px] h-[40px] rounded-full bg-gray-700 text-white font-bold ">
-                        {item.name[0]}
-                      </div>
-                    )
-                  ) : null}
-                  {item.category === "community" ? (
-                    item.communityPhoto !== "" ? (
-                      <div>
-                        <img
-                          className="w-[40px] h-[40px] rounded-full"
-                          src={item.communityPhoto}
-                          alt="loading"
-                        />
-                      </div>
-                    ) : (
-                      <div className="w-[40px] h-[40px] rounded-full bg-gray-700 text-white font-bold text-xl flex justify-center items-center ">
-                        /c
-                      </div>
-                    )
-                  ) : null}
-                  <div className="text-white font-bold ml-2 flex justify-center items-center">
-                    {item.category === "people"
-                      ? trimString(item.name)
-                      : trimString(item.title)}
+            {console.log(searchResult)}
+            {searchResult.length >= 1 &&
+              searchResult.map((item) => (
+                <Link
+                  key={item._id}
+                  to={
+                    item.category === "people"
+                      ? `/u/${item.name}`
+                      : `/c/${item._id}`
+                  }
+                >
+                  <div className="w-full h-fit p-1 hover:bg-[#464545] cursor-pointer flex rounded-md m-1">
+                    {item.category === "people" ? (
+                      item.photo !== "" ? (
+                        <div>
+                          <img
+                            className="w-[40px] h-[40px] rounded-full"
+                            src={item.photo}
+                            alt="loading"
+                          />
+                        </div>
+                      ) : (
+                        <div className="w-[40px] h-[40px] rounded-full bg-gray-700 text-white font-bold ">
+                          {item.name[0]}
+                        </div>
+                      )
+                    ) : null}
+                    {item.category === "community" ? (
+                      item.communityPhoto !== "" ? (
+                        <div>
+                          <img
+                            className="w-[40px] h-[40px] rounded-full"
+                            src={item.communityPhoto}
+                            alt="loading"
+                          />
+                        </div>
+                      ) : (
+                        <div className="w-[40px] h-[40px] rounded-full bg-gray-700 text-white font-bold text-xl flex justify-center items-center ">
+                          /c
+                        </div>
+                      )
+                    ) : null}
+                    <div className="text-white font-bold ml-2 flex justify-center items-center">
+                      {item.category === "people"
+                        ? trimString(item.name)
+                        : trimString(item.title)}
+                    </div>
+                    <div className="text-gray-400 font-playwright ml-1 text-[11px] flex items-center">
+                      {item.category}
+                    </div>
                   </div>
-                  <div className="text-gray-400 font-playwright ml-1 text-[11px] flex items-center">
-                    {item.category}
-                  </div>
-                </div>
-              </Link>
-            ))}
+                </Link>
+              ))}
           </div>
         )}
       </div>
